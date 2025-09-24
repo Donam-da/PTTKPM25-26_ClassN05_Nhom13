1da86accf92f8c120de3d233d8676513c90a87ca
Donam-da <22014002@st.phenikaa-uni.edu.vn>
2025-09-24 16:55:16 +0700

    Cập nhật cho tuần 5

--- SRC/Project/src/models/Dangky.js
+++ SRC/Project/src/models/Dangky.js
@@ -3,21 +3,63 @@ const sequelize = require('./database');
 const SinhVien = require('./sinhvien');
 const HocPhan = require('./HocPhan');
 
+const DangKyStates = Object.freeze({
+  KHOI_TAO: 'KHOI_TAO',
+  DANG_XU_LY: 'DANG_XU_LY',
+  THANH_CONG: 'THANH_CONG',
+  CHO_DOI: 'CHO_DOI',
+  TU_CHOI: 'TU_CHOI',
+  DA_HUY: 'DA_HUY'
+});
+
 const DangKy = sequelize.define('DangKy', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-    trangThai: DataTypes.STRING,
-    ngayDangKy: DataTypes.DATE
+    trangThai: { type: DataTypes.STRING, defaultValue: DangKyStates.KHOI_TAO },
+    ngayDangKy: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
 }, {
     tableName: 'dangky',
     timestamps: false
 });
+
+// Instance methods: state transitions
+DangKy.prototype.submit = function() {
+  if (this.trangThai !== DangKyStates.KHOI_TAO) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.DANG_XU_LY; return this.save();
+};
+DangKy.prototype.markValid = function() {
+  if (this.trangThai !== DangKyStates.DANG_XU_LY) throw new Error('Invalid state');
+  return Promise.resolve(this);
+};
+DangKy.prototype.confirm = function() {
+  if (this.trangThai !== DangKyStates.DANG_XU_LY) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.THANH_CONG; return this.save();
+};
+DangKy.prototype.waitlist = function() {
+  if (this.trangThai !== DangKyStates.DANG_XU_LY) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.CHO_DOI; return this.save();
+};
+DangKy.prototype.resumeFromWaitlist = function() {
+  if (this.trangThai !== DangKyStates.CHO_DOI) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.DANG_XU_LY; return this.save();
+};
+DangKy.prototype.reject = function(reason) {
+  if (this.trangThai !== DangKyStates.DANG_XU_LY) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.TU_CHOI; this.lyDoTuChoi = reason; return this.save();
+};
+DangKy.prototype.cancel = function() {
+  if (![DangKyStates.THANH_CONG, DangKyStates.CHO_DOI].includes(this.trangThai)) throw new Error('Invalid state');
+  this.trangThai = DangKyStates.DA_HUY; return this.save();
+};
 
 // Quan hệ: SinhVien - HocPhan (N-N)
 SinhVien.belongsToMany(HocPhan, { through: DangKy, foreignKey: 'maSV' });
 HocPhan.belongsToMany(SinhVien, { through: DangKy, foreignKey: 'maHP' });
 
 module.exports = DangKy;
+
+
+module.exports.DangKyStates = DangKyStates;
--- SRC/Project/src/models/LopHocPhan.js
+++ SRC/Project/src/models/LopHocPhan.js
@@ -3,18 +3,62 @@ const sequelize = require('./database');
 const HocPhan = require('./HocPhan');
 const GiangVien = require('./GiangVien');
 
+const LopHocPhanStates = Object.freeze({
+  SAP_MO: 'SAP_MO',
+  DANG_MO: 'DANG_MO',
+  DA_DAY: 'DA_DAY',
+  DA_DONG: 'DA_DONG',
+  DA_HUY: 'DA_HUY'
+});
+
 const LopHocPhan = sequelize.define('LopHocPhan', {
-    maLHP: {
-        type: DataTypes.STRING,
-        primaryKey: true
-    },
+    maLHP: { type: DataTypes.STRING, primaryKey: true },
     thu: DataTypes.STRING,
     tietHoc: DataTypes.STRING,
-    phongHoc: DataTypes.STRING
+    phongHoc: DataTypes.STRING,
+    chiTieu: { type: DataTypes.INTEGER, defaultValue: 0 },
+    siSo: { type: DataTypes.INTEGER, defaultValue: 0 },
+    trangThai: { type: DataTypes.STRING, defaultValue: LopHocPhanStates.SAP_MO }
 }, {
     tableName: 'lophocphan',
     timestamps: false
 });
+
+// Helper & transitions
+LopHocPhan.prototype.conCho = function() {
+  return this.siSo < this.chiTieu;
+};
+LopHocPhan.prototype.openRegistration = function() {
+  if (this.trangThai !== LopHocPhanStates.SAP_MO) throw new Error('Invalid state');
+  this.trangThai = LopHocPhanStates.DANG_MO; return this.save();
+};
+LopHocPhan.prototype.closeRegistration = function() {
+  if (![LopHocPhanStates.DANG_MO, LopHocPhanStates.DA_DAY].includes(this.trangThai)) throw new Error('Invalid state');
+  this.trangThai = LopHocPhanStates.DA_DONG; return this.save();
+};
+LopHocPhan.prototype.cancelClass = function(reason) {
+  if (this.trangThai === LopHocPhanStates.DA_HUY) return Promise.resolve(this);
+  this.trangThai = LopHocPhanStates.DA_HUY; this.lyDoHuy = reason; return this.save();
+};
+LopHocPhan.prototype.themSinhVien = function() {
+  if (this.trangThai !== LopHocPhanStates.DANG_MO) throw new Error('Lớp chưa mở đăng ký');
+  if (!this.conCho()) throw new Error('Lớp đã đầy');
+  this.siSo += 1;
+  if (!this.conCho()) this.trangThai = LopHocPhanStates.DA_DAY;
+  return this.save();
+};
+LopHocPhan.prototype.xoaSinhVien = function() {
+  if (this.siSo > 0) this.siSo -= 1;
+  if (this.trangThai === LopHocPhanStates.DA_DAY && this.conCho()) this.trangThai = LopHocPhanStates.DANG_MO;
+  return this.save();
+};
+LopHocPhan.prototype.setChiTieu = function(newQuota) {
+  if (newQuota <= 0) throw new Error('Chỉ tiêu phải > 0');
+  this.chiTieu = newQuota;
+  if (this.trangThai === LopHocPhanStates.DA_DAY && this.conCho()) this.trangThai = LopHocPhanStates.DANG_MO;
+  return this.save();
+};
+
 
 // Quan hệ
 HocPhan.hasMany(LopHocPhan, { foreignKey: 'maHP' });
@@ -24,3 +68,6 @@ GiangVien.hasMany(LopHocPhan, { foreignKey: 'maGV' });
 LopHocPhan.belongsTo(GiangVien, { foreignKey: 'maGV' });
 
 module.exports = LopHocPhan;
+
+
+module.exports.LopHocPhanStates = LopHocPhanStates;
